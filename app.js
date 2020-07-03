0044ad6f646a633c2ffb1e8be1139d09e3c5564f
golovan8672 <golovan8672@gmail.com>
2020-07-03 08:28:35 +0500

    fixed heroku

--- app.js
+++ app.js
@@ -14,12 +14,18 @@ app.use('/api/auth', require('./routes/auth.routes'))
 
 
 
- mongoose.connect(process.env.MONGODB_URI,{
+async function start(){
+    try {
+        await mongoose.connect(process.env.MONGODB_URI,{
             useNewUrlParser: true,
             useUnifiedTopology: true,
             useCreateIndex: true,
         })
+    app.listen(port, () => console.log(`Server has been started on port ${port}`))
 
-app.listen(port, () => console.log(`Server has been started on port ${port}`))
-
- 
+    } catch(e){
+        console.log('Server Error',e.message)
+        process.exit(1)
+    }
+}
+start()
