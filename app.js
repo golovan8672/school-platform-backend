45051fd88b17686797d538f7bf85fe7ff516c71f
golovan8672 <golovan8672@gmail.com>
2020-07-03 10:36:01 +0500

    mongodb

--- app.js
+++ app.js
@@ -8,14 +8,16 @@ const port = process.env.PORT || 80
 app.use(express.json({ extended: true }))
 
 app.use('/api/auth', require('./routes/auth.routes'))
+const mongoURI = "mongodb+srv://golovan8672:xum01KnhZhrjQx7U@cluster0.iybeo.azure.mongodb.net/app?retryWrites=true&w=majority"
 
+app.listen(port, () => console.log(`Server has been started on port ${port}`))
 
-mongoose.connect(process.env.MONGODB_URI, {
+mongoose.connect(process.env.MONGODB_URI || mongoURI, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
     useCreateIndex: true,
 })
 
-app.listen(port, () => console.log(`Server has been started on port ${port}`))
+
 
 
