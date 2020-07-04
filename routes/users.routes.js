83bbcce84ca57d6e5d2334b025358f0435cc2df4
golovan8672 <golovan8672@gmail.com>
2020-07-04 18:10:16 +0500

    add users

--- routes/users.routes.js
+++ routes/users.routes.js
@@ -6,9 +6,7 @@ const Student = require('../models/Student')
 router.get('/students',function(req,res){
 	Student.find(function(err,students){
 		if(err)
-			res.send(err);
-        const {role,email,login,mobileNumber,fio,classroom} = students
-        res.status(200).json(role,email,login,mobileNumber,fio,classroom);
+        res.status(200).json(students);
 	});
 });
 
