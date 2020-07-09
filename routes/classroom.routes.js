dc8ccbaa579f12a2f3b67ac09ed6133340aac404
golovan8672 <golovan8672@gmail.com>
2020-07-09 12:38:07 +0500

    classroom routes

--- routes/classroom.routes.js
+++ routes/classroom.routes.js
@@ -3,18 +3,28 @@ const router = Router()
 const Classroom= require('../models/Classroom')
 
 
+router.get("/classrooms", async (req, res) => {
+    const classrooms = await Classroom.find().select("-__v");
+    res.send(classrooms);
+});
+router.get("/classrooms/:id", async (req, res) => {
+    const classroom = await Classroom.findById(req.params.id)
+    res.send(classroom);
+});
 router.post("/setClassroom",async(req,res)=>{
     try{
-        const classroom = new Classroom(req.body)
-        await classroom.save()
+        const newClassroom = new Classroom(req.body)
+        await newClassroom.save()
         res.status(201).json({message: "Класс создан",resultCode: 0})
     } catch (e){
         res.status(500).json({message : 'Что-то пошло не так, попробуйте снова'})
     }
 
 });
-
-//
+router.delete('/deleteClassroom/:id', async (req, res) => {
+    await Classroom.findByIdAndRemove({ _id: req.params.id })
+    res.send("Класс удален!")
+})
 
 
 
