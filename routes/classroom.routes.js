3d268491edde053f3c58cfa41de13fbc4a12d500
golovan8672 <golovan8672@gmail.com>
2020-07-11 04:07:46 +0500

    eg

--- routes/classroom.routes.js
+++ routes/classroom.routes.js
@@ -26,15 +26,14 @@ router.delete('/classrooms/:id/deleteStudent', async (req, res) => {
 router.post("/classrooms/:id/addStudent", async (req, res) => {
     const classroom = await Classroom.findById(req.params.id);
     const {studentId,fio} = req.body;
-    const student = await classroom.students.indexOf({studentId},0)
-
+    const student = await classroom.students.indexOf({studentId})
     if (student === -1) {
-        return res.status(202).json({message: 'Студент уже входит в состав класса',resultCode: 1})
-    }
+        classroom.students.push(req.body);
+        await classroom.save();
+        res.status(200).json({ message: "Ученик добавлен!", resultCode: 0 })
+    } else  return res.status(202).json({message: 'Студент уже входит в состав класса',resultCode: 1})
 
-    classroom.students.push(req.body);
-    await classroom.save();
-    res.status(200).json({ message: "Ученик добавлен!", resultCode: 0 })
+   
 });
 router.put("/classrooms/:id/addTeacher", async (req, res) => {
     await Classroom.findByIdAndUpdate(req.params.id, req.body)
