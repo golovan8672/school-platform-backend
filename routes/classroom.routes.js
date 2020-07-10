018dfdbb0befd08ff7d4d130c65fceecfd688722
golovan8672 <golovan8672@gmail.com>
2020-07-11 04:39:05 +0500

    eg

--- routes/classroom.routes.js
+++ routes/classroom.routes.js
@@ -2,7 +2,7 @@ const { Router } = require('express')
 const router = Router()
 const Classroom = require('../models/Classroom')
 
-//Получение списка классов и класса 
+// Получение списка классов и класса 
 
 router.get("/classrooms", async (req, res) => {
     const classrooms = await Classroom.find().select("-__v");
@@ -13,10 +13,19 @@ router.get("/classrooms/:id", async (req, res) => {
     res.send(classroom);
 });
 
-//Удаление ученика из класса
-router.delete('/classrooms/:id/deleteStudent', async (req, res) => {
-    const classroom = await Classroom.findById({ _id: req.params.id })
-    classroom.students.remove(req.body)
+
+router.delete('/classrooms/:parentId/deleteForumMessage/:id', async (req, res) => {
+    const classroom = await Classroom.findById({ _id: req.params.parentId })
+    classroom.students.remove(req.body.id)
+    await classroom.save()
+    res.status(200).json({ message: "Ученик удален из класса!" })
+})
+
+// Удаление ученика из класса
+
+router.delete('/classrooms/:parentId/deleteStudent/:id', async (req, res) => {
+    const classroom = await Classroom.findById({ _id: req.params.parentId })
+    classroom.students.remove(req.body.id)
     await classroom.save()
     res.status(200).json({ message: "Ученик удален из класса!" })
 })
@@ -25,19 +34,16 @@ router.delete('/classrooms/:id/deleteStudent', async (req, res) => {
 
 router.post("/classrooms/:id/addStudent", async (req, res) => {
     const classroom = await Classroom.findById(req.params.id);
-    const {studentId,fio} = req.body;
-    const student = await classroom.students.includes({studentId})
-    if (student === false) {
-        classroom.students.push(req.body);
-        await classroom.save();
-        res.status(200).json({ message: "Ученик добавлен!", resultCode: 0 })
-    } else  return res.status(202).json({message: 'Студент уже входит в состав класса',resultCode: 1})
 
-   
+    classroom.students.push(req.body);
+    await classroom.save();
+
+    res.status(200).json({ message: "Ученик добавлен!", resultCode: 0 })
+
 });
 router.put("/classrooms/:id/addTeacher", async (req, res) => {
     await Classroom.findByIdAndUpdate(req.params.id, req.body)
-    res.status(200).json({message: "Классный руководитель обновлен!"})
+    res.status(200).json({ message: "Классный руководитель обновлен!" })
 });
 
 // Добавление и удаление классов
