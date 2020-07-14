4abf828bb2cf320ad39f7610350549a6b9f21c9e
golovan8672 <golovan8672@gmail.com>
2020-07-14 14:28:50 +0500

    add methodes

--- routes/classroom.routes.js
+++ routes/classroom.routes.js
@@ -13,23 +13,6 @@ router.get("/classrooms/:id", async (req, res) => {
     res.send(classroom);
 });
 
-
-router.delete('/classrooms/:parentId/deleteForumMessage/:id', async (req, res) => {
-    const classroom = await Classroom.findById({ _id: req.params.parentId })
-    classroom.students.remove(req.body.id)
-    await classroom.save()
-    res.status(200).json({ message: "Ученик удален из класса!" })
-})
-
-// Удаление ученика из класса
-
-router.delete('/classrooms/:parentId/deleteStudent/:id', async (req, res) => {
-    const classroom = await Classroom.findById({ _id: req.params.parentId })
-    classroom.students.id(req.body.id).remove()
-    await classroom.save()
-    res.status(200).json({ message: "Ученик удален из класса!" })
-})
-
 // Добавление учителя и студента
 
 router.delete("/classrooms/:id/deleteMessage", async (req, res) => {
