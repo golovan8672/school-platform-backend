abfcc69825487665c976aa68b8b821f5f590757b
golovan8672 <golovan8672@gmail.com>
2020-07-15 13:10:09 +0500

    123123

--- models/Classroom.js
+++ models/Classroom.js
@@ -1,7 +1,11 @@
 const { Schema, model } = require('mongoose')
 const classroomSchema = new Schema({
     classNumber: {type: String, required: true},
-    classForumMessages: [classForumMessage],
+    classForumMessages: [{
+        author: {type: String, required: true},
+        date: {type: Date, default: Date.now},
+        message: {type: String, required: true}
+    }],
     students: [{_id: false, studentId: {type: String}}],
     classTeacher: {type: String, default: 'none'},
     students: [{ _id: false, studentId: { type: String }, fio: {type: String} }],
