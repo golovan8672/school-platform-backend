c11735ca127094e9717867a764d4d3c21d3de5ec
golovan8672 <golovan8672@gmail.com>
2020-07-31 19:21:37 +0500

    add homework

--- models/Homework.js
+++ models/Homework.js
@@ -6,6 +6,7 @@ const homeworkSchema = new Schema({
     publicDate: { type: String, required: true },
     subject: { type: String, required: true },
     teacher: { type: String, required: true },
+    taskTitle: {type: String, required: true},
     answerToTask: { type: String, required: true },
     deadlineDate: {type: String, required: true},
     mark: {type: String, enum: ["","2","3","4","5"], default: ""},
