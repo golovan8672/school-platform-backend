9223890db0085c2cc2155ca369fbd0882e22ef0f
golovan8672 <golovan8672@gmail.com>
2020-08-06 14:42:09 +0500

    add file routes

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
+require('dotenv/config')
 const express = require('express')
+const multer = require('multer')
 const mongoose = require('mongoose')
 const cors = require('cors')
 
@@ -8,6 +10,15 @@ app.use(cors({credentials: true, origin: true}))
 
 const port = process.env.PORT || 80
 
+const storage = multer = multer.memoryStorage({
+    destination: function(req,file,callback){
+        callback(null,'')
+    }
+})
+
+export const upload = multer({storage}).single('image')
+
+
 app.use(express.json({ extended: true }))
 
 app.use('/api/auth', require('./routes/auth.routes'))
@@ -16,6 +27,7 @@ app.use('/api/classroom', require('./routes/classroom.routes'))
 app.use('/api/schedule', require('./routes/schedule.routes'))
 app.use('/api/task', require('./routes/task.routes'))
 app.use('/api/homework', require('./routes/homework.routes'))
+app.use('/api/file', require('./routes/file.routes'))
 
 mongoose.connect(process.env.MONGODB_URL,{
     useNewUrlParser: true,
