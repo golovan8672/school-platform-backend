7250187350e66526433918546cff739de8bcb6c9
golovan8672 <golovan8672@gmail.com>
2020-08-07 00:25:05 +0500

    123

--- app.js
+++ app.js
@@ -1,5 +1,9 @@
+require('dotenv/config')
+
 const express = require('express')
 const multer = require('multer')
+const AWS = require('aws-sdk')
+const uuid = require('uuid/v4')
 const mongoose = require('mongoose')
 const cors = require('cors')
 
@@ -8,6 +12,41 @@ const app = express()
 app.use(cors({credentials: true, origin: true}))
 
 const port = process.env.PORT || 80
+
+const s3 = new AWS.S3({
+    accessKeyId: process.env.AWS_ID,
+    secretAccessKey: process.env.AWS_SECRET
+})
+
+const upload = multer({storage}).single('image')
+
+app.post('/api/upload',upload,(req, res) => {
+
+    let myFile = req.file.originalname.split(".")
+    const fileType = myFile[myFile.length - 1]
+
+    const params = {
+        Bucket: process.env.AWS_BUCKET_NAME,
+        Key: `${uuid()}.${fileType}`,
+        Body: req.file.buffer
+    }
+
+    s3.upload(params, (error, data) => {
+        if(error){
+            res.status(500).send(error)
+        }
+
+        res.status(200).send(data)
+    })
+})
+
+
+const storage = multer.memoryStorage({
+    destination: function(req, file, callback) {
+        callback(null, '')
+    }
+})
+
 
 app.use(express.json({ extended: true }))
 
